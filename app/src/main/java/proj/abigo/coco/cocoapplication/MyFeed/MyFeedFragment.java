31e8228d5fdf0790eb3a8317a9f9a4f1557ca6c7
eun-bi <uko02111@naver.com>
2018-02-20 04:00:24 +0900

    MyFeed, MyPage 탭 생성 + Fragment 적용

---
+++ app/src/main/java/proj/abigo/coco/cocoapplication/MyFeed/MyFeedFragment.java
@@ -0,0 +1,16 @@
+package proj.abigo.coco.cocoapplication.MyFeed;
+
+import android.support.v4.app.Fragment;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by User on 2018-02-20.
+ */
+
+public class MyFeedFragment extends Fragment implements View.OnTouchListener{
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+}
